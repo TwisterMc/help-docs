f274fe4005a036467d8379041cce905e9edf82f5
Thomas McMahon <tmcmahon@nerdery.com>
2019-10-22 20:56:48 -0500

    re-working the output of pages so we can get childrend of children and so on

--- inc/class-help-docs.php
+++ inc/class-help-docs.php
@@ -122,27 +122,17 @@ class Help_Docs {
 			echo '<p><a href="/wp-admin/post-new.php?post_type=help_docs" class="button button-large">' . esc_html( __( 'New Help Doc' ) ) . '</a></p>';
 			echo '<ul class="help_pages">';
 
-				$args  = array(
-					'sort_order'   => 'asc',
-					'sort_column'  => 'post_title',
-					'hierarchical' => 1,
-					'post_type'    => 'help_docs',
-				);
-				$pages = get_pages( $args );
+			$walker = new Help_Docs_Walker();
 
-				foreach ( $pages as $page ) {
+			$args = array(
+				'title_li'    => '',
+				'sort_column' => 'menu_order',
+				'post_type'   => 'help_docs',
+				'walker'      => $walker,
+			);
 
-					// TODO:  Check for no page.
-
-					$parent_class = '';
-					if ( 0 !== $page->post_parent ) {
-						$parent_class = 'child';
-					}
-					?>
-					<li class="<?php echo esc_attr( $parent_class ); ?>"><a href="/wp-admin/admin.php?page=help-docs-info.php&id=<?php echo esc_attr( $page->ID ); ?>"><?php echo esc_attr( $page->post_title ); ?></a></li>
-					<?php
-				}
-				echo '</ul>';
+			wp_list_pages( $args );
+			echo '</ul>';
 	}
 
 	/**
@@ -151,6 +141,8 @@ class Help_Docs {
 	 * @var array $_GET is used to pass in the help document post id
 	 */
 	public static function help_docs_admin_page_info() {
+		$admin_menu_title = self::help_docs_admin_menu_title();
+
 		?>
 		<div class="help-docs-wrapper">
 			<h2><?php echo esc_html( $admin_menu_title ); ?></h2>
@@ -184,5 +176,99 @@ class Help_Docs {
 		</div>
 
 		<?php
-	} 
+	}
+}
+
+/**
+ * Help_Docs Walker Class
+ * Used to modify the links as I'm not using the permalink structure.
+ * via: https://gist.github.com/donchenko/4ac5f1380ae687f11bfd13f379531f60
+ */
+class Help_Docs_Walker extends Walker_Page {
+	public function start_lvl( &$output, $depth = 0, $args = array() ) {
+		$indent = str_repeat( "\t", $depth );
+		$output .= "\n$indent<ul class='dropdown-menu' role='menu'>\n";
+	}
+
+	public function start_el( &$output, $page, $depth = 0, $args = array(), $current_page = 0 ) {
+		if ( $depth ) {
+			$indent = str_repeat( "\t", $depth );
+		} else {
+			$indent = '';
+		}
+
+		$css_class = array( 'page_item', 'page-item-' . $page->ID );
+
+		if ( isset( $args['pages_with_children'][ $page->ID ] ) ) {
+			$css_class[] = 'page_item_has_children';
+		}
+
+		if ( ! empty( $current_page ) ) {
+			$_current_page = get_post( $current_page );
+			if ( in_array( $page->ID, $_current_page->ancestors ) ) {
+				$css_class[] = 'current_page_ancestor';
+			}
+			if ( $page->ID === $current_page ) {
+				$css_class[] = 'current_page_item';
+			} elseif ( $_current_page && $page->ID === $_current_page->post_parent ) {
+				$css_class[] = 'current_page_parent';
+			}
+		} elseif ( get_option( 'page_for_posts' ) === $page->ID ) {
+			$css_class[] = 'current_page_parent';
+		}
+
+		/**
+		 * Filter the list of CSS classes to include with each page item in the list.
+		 *
+		 * @since 2.8.0
+		 *
+		 * @see wp_list_pages()
+		 *
+		 * @param array $css_class An array of CSS classes to be applied to each list item.
+		 * @param WP_Post $page Page data object.
+		 * @param int $depth Depth of page, used for padding.
+		 * @param array $args An array of arguments.
+		 * @param int $current_page ID of the current page.
+		 */
+		$css_classes = implode( ' ', apply_filters( 'page_css_class', $css_class, $page, $depth, $args, $current_page ) );
+
+		if ( '' === $page->post_title ) {
+			$page->post_title = sprintf( __( '#%d (no title)' ), $page->ID );
+		}
+
+		$args['link_before'] = empty( $args['link_before'] ) ? '' : $args['link_before'];
+		$args['link_after']  = empty( $args['link_after'] ) ? '' : $args['link_after'];
+
+		/** This filter is documented in wp-includes/post-template.php */
+		if ( isset( $args['pages_with_children'][ $page->ID ] ) ) {
+			$output .= $indent . sprintf(
+				'<li class="%s"><a href="%s" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">%s%s%s <span class="caret"></span></a>',
+				$css_classes,
+				'/wp-admin/admin.php?page=help-docs-info.php&id=' . $page->ID ,
+				$args['link_before'],
+				apply_filters( 'the_title', $page->post_title, $page->ID ),
+				$args['link_after']
+			);
+		} else {
+			$output .= $indent . sprintf(
+				'<li class="%s"><a href="%s">%s%s%s</a>',
+				$css_classes,
+				'/wp-admin/admin.php?page=help-docs-info.php&id=' . $page->ID,
+				$args['link_before'],
+				apply_filters( 'the_title', $page->post_title, $page->ID ),
+				$args['link_after']
+			);
+		}
+
+		if ( ! empty( $args['show_date'] ) ) {
+			if ( 'modified' === $args['show_date'] ) {
+				$time = $page->post_modified;
+			} else {
+				$time = $page->post_date;
+			}
+
+			$date_format = empty( $args['date_format'] ) ? '' : $args['date_format'];
+			$output     .= ' ' . mysql2date( $date_format, $time );
+		}
+	}
 }
